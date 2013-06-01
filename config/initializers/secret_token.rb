ebecc67f3fffd167ea0606e1847dbbd5d65c5562
Thomas Harris <tkharris@gmail.com>
2013-06-01 15:04:12 -0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pghdatahub::Application.config.secret_token = '05ff3910dac310d410e562647dabe63401c4eb7a143ba73b4486f358770b930e9fc9431f7aa5b0b1609aed20312bc6a2d4493a329e4c662b9f510312c8bd0d79'
