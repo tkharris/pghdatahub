b2adff1284d126ad3ba4947a9143ab4b32d6bc74
Thomas Harris <tkharris@gmail.com>
2013-06-02 15:29:53 -0400

    homepage bottom and dataset items

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,11 +5,15 @@ class SearchController < ApplicationController
 
   def search
   	query = params[:q]
-  	url = "http://ec2-54-226-133-156.compute-1.amazonaws.com:8983/solr/select?q=#{CGI.escape(query)}&wt=json"
+  	url = "http://ec2-54-226-133-156.compute-1.amazonaws.com:8983/solr/select?q=docs:#{CGI.escape(query)}&wt=json"
   	puts url
   	response = HTTParty.get(url)
 	#puts response.body, response.code, response.message, response.headers.inspect
-	@search_result = response.body
+	@search_result = ActiveSupport::JSON.decode(response.body)['response']
+	puts @search_result
+	@search_result['docs'].each do |r|
+		puts r
+	end
   	respond_to do |format|
   		format.html
   		format.json { render :json => @search_result }
--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -1,3 +1,7 @@
 class DataSet < ActiveRecord::Base
   attr_accessible :name
+
+  def self.types do
+  	'xls', 'kmz', 'zip', 'txt', 'pdf'
+  end
 end
